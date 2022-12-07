b77b1a48ab3f08b35e8ee3995641f56fe96ced7a
Nils Gustav Stråbø <nst@equinor.com>
2022-12-07 08:48:21 +0100

    sql

--- compute/sql.go
+++ compute/sql.go
@@ -19,6 +19,7 @@ func doSqlQuery() {
 		token, err := os.ReadFile(os.Getenv("AZURE_FEDERATED_TOKEN_FILE"))
 		return string(token), err
 	})
+
 	authority := fmt.Sprintf("%s%s/oauth2/token", os.Getenv("AZURE_AUTHORITY_HOST"), os.Getenv("AZURE_TENANT_ID"))
 	fmt.Printf("using authority %s\n", authority)
 	confidentialClientApp, err := confidential.New(
@@ -31,6 +32,7 @@ func doSqlQuery() {
 		fmt.Println(err)
 		return
 	}
+
 	getToken := func(ctx context.Context) (string, error) {
 		var authResult confidential.AuthResult
 		authResult, err := confidentialClientApp.AcquireTokenSilent(ctx, scopes)
@@ -46,7 +48,7 @@ func doSqlQuery() {
 		return authResult.AccessToken, nil
 	}
 	connector, err := mssql.NewSecurityTokenConnector(
-		msdsn.Config{Host: os.Getenv("SERVER_NAME"), Database: os.Getenv("DATABASE_NAME")},
+		msdsn.Config{Host: os.Getenv("SQL_SERVER_NAME"), Database: os.Getenv("SQL_DATABASE_NAME")},
 		getToken)
 
 	if err != nil {
@@ -56,25 +58,24 @@ func doSqlQuery() {
 	db := sql.OpenDB(connector)
 	defer db.Close()
 
-	
-		func() {
-			conn, err := db.Conn(context.Background())
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer conn.Close()
-			rows, err := db.QueryContext(context.Background(), "select count(1) as cnt from dbo.Vulnerability")
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			defer rows.Close()
-			var rowcount int64
-			for rows.Next() {
-				rows.Scan(&rowcount)
-				fmt.Printf("%v: there are %d rows in dbo.Vulnerability\n", time.Now(), rowcount)
-			}
-		}()
-	
+	func() {
+		conn, err := db.Conn(context.Background())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer conn.Close()
+		rows, err := db.QueryContext(context.Background(), "select count(1) as cnt from dbo.Products")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer rows.Close()
+		var rowcount int64
+		for rows.Next() {
+			rows.Scan(&rowcount)
+			fmt.Printf("%v: there are %d rows in dbo.Products\n", time.Now(), rowcount)
+		}
+	}()
+
 }
