fb0e1dab0da7dde9b062c1730a337900dbd6dd6e
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-04-10 12:42:23 +0200

    fdsfsdfs

--- echoweb/main.go
+++ echoweb/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -16,10 +18,14 @@ func main() {
 	}
 }
 
+var (
+	responsePart string
+)
+
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s - %v", r.RemoteAddr, r.URL)
+	fmt.Printf("%s - %v\n", r.RemoteAddr, r.URL)
 	r.ParseForm()
-	if codeStr := r.Form.Get("code"); len(codeStr) > 0 {
+	if codeStr := r.URL.Query().Get("code"); len(codeStr) > 0 {
 		if code, err := strconv.Atoi(codeStr); err == nil {
 			w.WriteHeader(code)
 			w.Write([]byte(fmt.Sprintf("Returned code %v", code)))
@@ -27,5 +33,33 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	w.WriteHeader(200)
-	w.Write([]byte("all good"))
+	w.Write([]byte("all good\n"))
+
+	responseKb := 1
+	if v := r.URL.Query().Get("size"); len(v) > 0 {
+		if n, err := strconv.Atoi(v); err == nil {
+			responseKb = n
+		}
+	}
+	sleep := 0
+	if v := r.URL.Query().Get("sleep"); len(v) > 0 {
+		if n, err := strconv.Atoi(v); err == nil {
+			sleep = n
+		}
+	}
+	fmt.Printf("writing %v KB and sleeping %v seconds between each KB", responseKb, sleep)
+	for range responseKb {
+		w.Write([]byte(strings.Repeat(responsePart, 16)))
+		w.(http.Flusher).Flush()
+		time.Sleep(time.Duration(sleep) * time.Second)
+	}
+}
+
+func init() {
+	var sb strings.Builder
+
+	for i := range 64 {
+		sb.WriteByte(byte(i + 65))
+	}
+	responsePart = sb.String()
 }
